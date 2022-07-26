16352ee159fa35383bff456fa8cf0f7d0a3cb893
Sevann Triztan <striztan@dacgroup.com>
2022-07-26 17:19:35 -0300

    Context factory

---
+++ GloboTicket.API/DesignTime/DesignTimeModelConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using GloboTicket.SharedKernel.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.API.DesignTime
+{
+    internal class DesignTimeModelConfiguration : IModelConfiguration
+    {
+        public void ConfigureModel(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ GloboTicket.API/DesignTime/GloboTicketContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using GloboTicket.Domain;
+using GloboTicket.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace GloboTicket.API.DesignTime
+{
+    public class GloboTicketContextFactory : IDesignTimeDbContextFactory<GloboTicketContext>
+    {
+        private const string AdminConnectionString = "GLOBOTICKET_ADMIN_CONNECTION_STRING";
+
+        public GloboTicketContext CreateDbContext(string[] args)
+        {
+            string? connectionString = Environment.GetEnvironmentVariable(AdminConnectionString);
+            
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ApplicationException($"Please set the environment variable {AdminConnectionString}");
+            }
+            
+            DbContextOptions<GloboTicketContext>? options = new DbContextOptionsBuilder<GloboTicketContext>()
+                .UseSqlServer(connectionString, sqlOptions =>
+                {
+                    sqlOptions.MigrationsAssembly(typeof(ServiceRegistration).Assembly.FullName);
+                })
+                .Options;
+            return new GloboTicketContext(options, new DesignTimeModelConfiguration());
+        }
+    }
+}
